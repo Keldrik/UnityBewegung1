327bb9a70d5c03d8db904453448e634019df0e6b
Thomas Wiegold <mail@thomas-wiegold.com>
2014-06-30 20:12:32 +0200

    Bewegen Script + Scene

---
+++ Assets/Script/Bewegen.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bewegen : MonoBehaviour
+{
+    public float Geschwindigkeit;
+    public float Beschleunigung;
+    public float Rutschen;
+
+    float aktuelleGeschwindigkeit = 0;
+    Vector3 aktuelleRichtung = Vector3.zero;
+    float rutschenGeschwindigkeit = 0;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (EingabeÜberprüfen())
+        {
+            if (aktuelleGeschwindigkeit < Geschwindigkeit)
+            {
+                aktuelleGeschwindigkeit += Beschleunigung * Time.deltaTime;
+            }
+            this.transform.position += aktuelleRichtung * aktuelleGeschwindigkeit * Time.deltaTime;
+            rutschenGeschwindigkeit = aktuelleGeschwindigkeit / 2;
+        }
+        else
+        {
+            if (rutschenGeschwindigkeit > 0)
+            {
+                this.transform.position += aktuelleRichtung * rutschenGeschwindigkeit * Time.deltaTime;
+                rutschenGeschwindigkeit -= (100f / Rutschen) * Time.deltaTime;
+            }
+            aktuelleGeschwindigkeit = 0f;
+        }
+    }
+
+    private bool EingabeÜberprüfen()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            aktuelleRichtung = new Vector3(1f, 0f, 0f);
+            return true;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            aktuelleRichtung = new Vector3(-1f, 0f, 0f);
+            return true;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            aktuelleRichtung = new Vector3(0f, 0f, 1f);
+            return true;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            aktuelleRichtung = new Vector3(0f, 0f, -1f);
+            return true;
+        }
+        return false;
+    }
+}
--- Assets/Script/Teleportieren.cs
+++ Assets/Script/Teleportieren.cs
@@ -1,83 +1,38 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Teleportieren : MonoBehaviour
 {
+    private GameObject[] wegpunkte = new GameObject[4];
 
-    // GameOject Array für die 4 Wegpunkte in der Scene
-    GameObject[] wegpunkte = new GameObject[4];
+    private GameObject letzterWegpunkt;
 
-
-    // Der Wegpunkt zu dessen Position das Testobjekt zuletzt teleportiert wurde, zum Start null
-    GameObject letzterWegpunkt;
-
-
-
-
-	// Start der Scene
-	void Start ()
+    private void Start()
     {
-
-        // For Schleife wird so oft durchlaufen, wie das wegpunkte Array lang ist.
         for (int i = 0; i < wegpunkte.Length; i++)
         {
-
-
-            // Es wird ein GameObject gesucht das den Namen "Wegpunkt" plus den aktuellen Index + 1 hat und dann dem entsprechenden Index im Array zugewiesen.
-            // Die Schleife und das Array fangen mit der Zählung bei 0 an, die Wegpunkte in der Scene bei 1.
-
             wegpunkte[i] = GameObject.Find("Wegpunkt" + (i + 1).ToString());
-
         }
-	}
-	
+    }
 
-
-	// Wird bei jedem Frame ausgeführt
-	void Update ()
+    private void Update()
     {
-
-
-        // Überprüft ob die "Space" Taste gedrückt wird.
-
         if (Input.GetKeyDown("space"))
         {
             WegpunktSuchenUndPositionSetzen();
         }
-	}
 
+        this.transform.position.Set(1f, 0f, 5f);
+    }
 
-
-    void WegpunktSuchenUndPositionSetzen()
+    private void WegpunktSuchenUndPositionSetzen()
     {
-
-        // Random.Range gibt einen zufälligen Int Zahlenwert zurück, der zwischen 0 und der Länge des wegpunkte Array liegt.
-        // Mit dem Ergebnis bestimmen wir, welchen Index aus den Array wir haben wollen und weisen das Objekt der neuerwegpunkt Variable zu.
-
         GameObject neuerWegpunkt = wegpunkte[Random.Range(0, wegpunkte.Length)];
-
-
-
-
-
-        // Überprüft ob das neuerWegpunkt zugewiesene Objekt NICHT das gleiche ist wie letzterWegpunkt
 
         if (!neuerWegpunkt.Equals(letzterWegpunkt))
         {
-
-            // "Teleportieren":
-            // Der Vector3 position der Transform Komponente des neuerWegpunkt Object wird dem Script ausführenden GameObject zugewiesen.
             this.transform.position = neuerWegpunkt.transform.position;
-
-
-
-            // Damit wir beim nächsten mal wissen wo wir zuletzt gewesen sind, wird letzterWegpunkt das Wegpunkt Object von neuerWegpunkt zugewiesen.
             letzterWegpunkt = neuerWegpunkt;
         }
-
-
-
-        // Wenn neuerWegpunkt und letzterWegpunkt gleich sind, wird die Methode nochmal ausgeführt.
         else
         {
             WegpunktSuchenUndPositionSetzen();
